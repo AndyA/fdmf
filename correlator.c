13636c90db324b797f5904dca75716e2be01e97e
Andy Armstrong <andy@hexten.net>
2011-01-30 17:30:13 +0000

    Compute correlation between file pairs. This implementation is O(N^2) but then so is vector_pairs.

--- correlator.c
+++ correlator.c
@@ -5,6 +5,7 @@
  */
 
 #include <ctype.h>
+#include <limits.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,11 +14,20 @@
 #define HASH_LEN 768
 #define HASH_BYTES (HASH_LEN / 8)
 #define HASH_CHARS (HASH_LEN / 4)
+#define SUMMARY_LEN 8
+#define SUMMARY_SPAN (HASH_BYTES/SUMMARY_LEN)
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 struct phash {
   struct phash *next;
-  unsigned char *bits;
-  unsigned int bit_count;
+  unsigned char bits[HASH_BYTES];
+  unsigned int summary[SUMMARY_LEN];
+};
+
+struct correlation {
+  const struct phash *pair[2];
+  unsigned int distance;
 };
 
 static void
@@ -41,9 +51,9 @@ count_bits( unsigned int v ) {
 }
 
 static unsigned int
-count_hash_bits( unsigned char *hash ) {
+count_hash_bits( unsigned char *hash, size_t bytes ) {
   unsigned int i, c = 0;
-  for ( i = 0; i < HASH_BYTES; i++ ) {
+  for ( i = 0; i < bytes; i++ ) {
     c += count_bits( hash[i] );
   }
   return c;
@@ -58,23 +68,20 @@ safe_malloc( size_t size ) {
   return m;
 }
 
-static unsigned char *
-read_hash( FILE * fl ) {
-  unsigned char hash[HASH_BYTES];
+static int
+read_hash( FILE * fl, unsigned char hash[HASH_BYTES] ) {
   memset( hash, 0, HASH_BYTES );
   int hp = 0;
   for ( ;; ) {
     int v, c = fgetc( fl );
     if ( c == EOF ) {
-      return NULL;
+      return 0;
     }
     if ( c == '\n' ) {
       if ( hp != HASH_CHARS ) {
         die( "Hash too short" );
       }
-      unsigned char *h = safe_malloc( HASH_BYTES );
-      memcpy( h, hash, HASH_BYTES );
-      return h;
+      return 1;
     }
     if ( c < 0 || c > 255 || !isxdigit( c ) ) {
       die( "Illegal character '%c' in data", c );
@@ -87,21 +94,25 @@ read_hash( FILE * fl ) {
 
 static struct phash *
 new_phash( unsigned char *hash, struct phash *next ) {
+  int i = 0;
   struct phash *ph = safe_malloc( sizeof( struct phash ) );
   ph->next = next;
-  ph->bits = hash;
-  ph->bit_count = count_hash_bits( hash );
+  memcpy( ph->bits, hash, HASH_BYTES );
+  for ( i = 0; i < SUMMARY_LEN; i++ ) {
+    ph->summary[i] =
+        count_hash_bits( ph->bits + SUMMARY_SPAN * i, SUMMARY_SPAN );
+  }
   return ph;
 }
 
 static struct phash *
 read_file( FILE * fl ) {
   struct phash *data = NULL;
+  unsigned char hash[HASH_BYTES];
   for ( ;; ) {
-    unsigned char *h = read_hash( fl );
-    if ( !h )
+    if ( !read_hash( fl, hash ) )
       break;
-    data = new_phash( h, data );
+    data = new_phash( hash, data );
   }
   return data;
 }
@@ -111,14 +122,13 @@ free_phash( struct phash *ph ) {
   struct phash *next;
   while ( ph ) {
     next = ph->next;
-    free( ph->bits );
     free( ph );
     ph = next;
   }
 }
 
 static void
-hexdump( unsigned char *data, size_t len ) {
+hexdump( const unsigned char *data, size_t len ) {
   unsigned int i;
   for ( i = 0; i < len; i++ ) {
     printf( "%02x", data[i] );
@@ -126,19 +136,149 @@ hexdump( unsigned char *data, size_t len ) {
 }
 
 static void
-dump_phash( struct phash *ph ) {
+dump_phash( const struct phash *ph ) {
+  int i;
   while ( ph ) {
     hexdump( ph->bits, HASH_BYTES );
-    printf( " %3d\n", ph->bit_count );
+    for ( i = 0; i < SUMMARY_LEN; i++ ) {
+      printf( " %3d", ph->summary[i] );
+    }
+    printf( "\n" );
     ph = ph->next;
   }
 }
 
+static struct correlation *
+new_correlation( size_t nent ) {
+  struct correlation *c;
+  unsigned i;
+
+  c = safe_malloc( sizeof( struct correlation ) * nent );
+  for ( i = 0; i < nent; i++ ) {
+    c[i].pair[0] = c[i].pair[1] = NULL;
+    c[i].distance = UINT_MAX;
+  }
+  return c;
+}
+
+static void
+sanity_check( const struct correlation *c, size_t nused ) {
+  unsigned i;
+  for ( i = 1; i < nused; i++ ) {
+    if ( c[i - 1].distance > c[i].distance ) {
+      die( "distance out of order at %u\n", i );
+    }
+  }
+}
+
+static void
+insert_correlation( struct correlation *c, size_t nent, size_t * nused,
+                    const struct phash *this, const struct phash *that,
+                    unsigned distance ) {
+  unsigned lo, hi, mid = 0;
+  size_t nnew;
+  for ( lo = 0, hi = *nused; lo < hi; ) {
+    mid = ( lo + hi ) / 2;
+    if ( c[mid].distance < distance ) {
+      lo = mid + 1;
+    }
+    else if ( c[mid].distance > distance ) {
+      hi = mid;
+    }
+  }
+
+  nnew = MIN( nent, *nused + 1 );
+  memmove( &c[mid + 1], &c[mid], ( nnew - 1 - mid ) * sizeof( c[0] ) );
+  c[mid].pair[0] = this;
+  c[mid].pair[1] = that;
+  c[mid].distance = distance;
+  *nused = nnew;
+  sanity_check( c, *nused );
+}
+
+static void
+compute_bitcount( unsigned char *bitcount ) {
+  unsigned i;
+  for ( i = 0; i < 256; i++ ) {
+    bitcount[i] = count_bits( i );
+  }
+}
+
+static unsigned int
+hash_distance( const struct phash *pi, const struct phash *pj,
+               const unsigned char *bitcount ) {
+  unsigned i, distance = 0;
+  for ( i = 0; i < HASH_BYTES; i++ ) {
+    distance += bitcount[pi->bits[i] ^ pj->bits[i]];
+  }
+  return distance;
+}
+
+static unsigned int
+best_distance( const struct phash *pi, const struct phash *pj ) {
+  unsigned i, distance = 0;
+  for ( i = 0; i < SUMMARY_LEN; i++ ) {
+    distance += abs( ( int ) pi->summary[i] - ( int ) pj->summary[i] );
+  }
+  return distance;
+}
+
+static struct correlation *
+correlate( const struct phash *data, size_t nent, size_t * nused ) {
+  struct correlation *c = new_correlation( nent );
+  const struct phash *pi, *pj;
+  unsigned distance;
+  unsigned char bitcount[256];
+
+  *nused = 0;
+
+  compute_bitcount( bitcount );
+
+  /* O(N^2) :) */
+  for ( pi = data; pi; pi = pi->next ) {
+    for ( pj = pi->next; pj; pj = pj->next ) {
+      /* don't know if this summary stuff is worth the bother */
+      if ( *nused == nent
+           && best_distance( pi, pj ) >= c[*nused - 1].distance ) {
+        continue;
+      }
+      distance = hash_distance( pi, pj, bitcount );
+      if ( *nused < nent || distance < c[*nused - 1].distance ) {
+        insert_correlation( c, nent, nused, pi, pj, distance );
+      }
+    }
+  }
+  return c;
+}
+
 int
-main( void ) {
-  struct phash *data = read_file( stdin );
+main( int argc, char *argv[] ) {
+  struct phash *data;
+  struct correlation *c;
+  size_t nent = 100, nused;
+  unsigned i;
+
+  if ( argc < 2 ) {
+    data = read_file( stdin );
+  }
+  else {
+    FILE *fl = fopen( argv[1], "r" );
+    if ( !fl ) {
+      die( "Can't read %s", argv[1] );
+    }
+    data = read_file( fl );
+    fclose( fl );
+  }
 
   dump_phash( data );
+  c = correlate( data, nent, &nused );
+  for ( i = 0; i < nused; i++ ) {
+    printf( "%5u ", c[i].distance );
+    hexdump( c[i].pair[0]->bits, HASH_BYTES );
+    printf( " " );
+    hexdump( c[i].pair[1]->bits, HASH_BYTES );
+    printf( "\n" );
+  }
   free_phash( data );
 
   return 0;
