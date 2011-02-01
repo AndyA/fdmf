7532e8ad30f622ea8ff2daaecfee9665cff7b896
Andy Armstrong <andy@hexten.net>
2011-02-01 22:13:03 +0000

    Fix numerous errors in use-ogle.sh

--- use-ogle.sh
+++ use-ogle.sh
@@ -3,8 +3,10 @@
 host="ogle.vpn.hexten.net"
 fdmf="/home/andy/Works/Media/fdmf"
 
+set -x
 ./fdmf_dump > dump
-rsync -avzP dump andy@$host:$home
-ssh -C $host "cd $home/dups && ./fdmf_correlator -v --keep 10000 dump > dups"
-rsync -avzP andy@$host:$home/dups .
+rsync -avzP dump andy@$host:$fdmf
+ssh -C $host "cd $fdmf && ./fdmf_correlator -v --keep 10000 dump > dups"
+rsync -avzP andy@$host:$fdmf/dups .
 ./fdmf_report dups > report
+set +x
