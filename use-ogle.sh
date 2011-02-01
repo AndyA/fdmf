55d96fd950ca62b9592285cf6253efa8c3d68dc0
Andy Armstrong <andy@hexten.net>
2011-02-01 22:25:37 +0000

    Use our hostname as a suffix on temp files so we can run multiple jobs from different boxes without them trampling on each other's files.

--- use-ogle.sh
+++ use-ogle.sh
@@ -2,11 +2,12 @@
 
 host="ogle.vpn.hexten.net"
 fdmf="/home/andy/Works/Media/fdmf"
+suff=$(hostname -s)
 
 set -x
-./fdmf_dump > dump
-rsync -avzP dump andy@$host:$fdmf
-ssh -C $host "cd $fdmf && ./fdmf_correlator -v --keep 10000 dump > dups"
-rsync -avzP andy@$host:$fdmf/dups .
-./fdmf_report dups > report
+./fdmf_dump > dump.$suff
+rsync -avzP dump.$suff andy@$host:$fdmf
+ssh -C $host "cd $fdmf && ./fdmf_correlator -v --keep 10000 dump.$suff > dups.$suff"
+rsync -avzP andy@$host:$fdmf/dups.$suff .
+./fdmf_report dups.$suff > report
 set +x
