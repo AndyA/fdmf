e054b9bf37b9bb089bfbcfd3ab24873387a23ecd
Andy Armstrong <andy@hexten.net>
2011-02-02 00:26:14 +0000

    Update wrapper scripts to specify database name where necessary.

--- use-ogle.sh
+++ use-ogle.sh
@@ -3,11 +3,14 @@
 host="ogle.vpn.hexten.net"
 fdmf="/home/andy/Works/Media/fdmf"
 suff=$(hostname -s)
+db=db
 
 set -x
-./fdmf_dump > dump.$suff
+./fdmf_dump --db $db > dump.$suff
 rsync -avzP dump.$suff andy@$host:$fdmf
 ssh -C $host "cd $fdmf && git pull origin master && make && ./fdmf_correlator -v --keep 10000 dump.$suff > dups.$suff"
 rsync -avzP andy@$host:$fdmf/dups.$suff .
-./fdmf_report dups.$suff > report
+./fdmf_report --db $db dups.$suff > report
 set +x
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
