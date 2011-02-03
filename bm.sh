b9f41ad44ddb882a096e77f6f05d604e71b06fce
Andy Armstrong <andy@hexten.net>
2011-02-03 13:36:02 +0000

    Merge bm.sh from master.

--- bm.sh
+++ bm.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-time \
-  ./fdmf_correlator -v --keep 100000 wrk/sfx-dump > /dev/null
+branch=$(git status | perl -ne '/On\s+branch\s+(\S+)/ && print $1')
+echo "Testing $branch"
+bmdir="bm-$branch"
+mkdir -p $bmdir
+make clean && make OPTIMIZE="-fprofile-arcs -ftest-coverage"
+time ./fdmf_correlator -v --keep 100000 wrk/sfx-dump > /dev/null
+gcov fdmf_correlator.c
+mv *.gcov $bmdir
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
