e110d0ff80d6592507f1acc95d828b01f1a00eff
Andy Armstrong <andy@hexten.net>
2011-02-03 13:22:28 +0000

    Run gcov after benchmark run

--- bm.sh
+++ bm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-time \
-  ./fdmf_correlator -v --keep 100000 wrk/sfx-dump > /dev/null
+time ./fdmf_correlator -v --keep 100000 wrk/sfx-dump > /dev/null
+gcov fdmf_correlator.c
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
