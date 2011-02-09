b426f8ae4ca8bb756ad628dba00f65f757084ee1
Andy Armstrong <andy@hexten.net>
2011-02-09 00:10:56 +0000

    Initial support for threadsafe mode.
    Conflicts:
    
    	tools/closure.c

--- tools/closure.h
+++ tools/closure.h
@@ -3,6 +3,10 @@
 #ifndef __BASENAME_H
 #define __BASENAME_H
 
+#if defined( THREADED_CLOSURES ) || defined( THREADED_NAME )
+#include <pthread.h>
+#endif
+
 /* <skip> */
 #define ALL_PROTO       int x, void *h
 #define CTX_PROTO       void *h
@@ -21,6 +25,18 @@ NAME new_NAME_cleanup( RETURN( *code ) ( ALL_PROTO ), CTX_PROTO,
                        void ( *cleanup ) ( CTX_PROTO ) );
 NAME new_NAME( RETURN( *code ) ( ALL_PROTO ), CTX_PROTO );
 void free_NAME( NAME cl );
+
+#if defined( THREADED_CLOSURES ) || defined( THREADED_NAME )
+
+NAME new_NAME_cleanup_nts( RETURN( *code ) ( ALL_PROTO ), CTX_PROTO,
+                           void ( *cleanup ) ( CTX_PROTO ) );
+NAME new_NAME_nts( RETURN( *code ) ( ALL_PROTO ), CTX_PROTO );
+void free_NAME_nts( NAME cl );
+
+NAME new_NAME_nb( RETURN( *code ) ( ALL_PROTO ), CTX_PROTO,
+                  void ( *cleanup ) ( CTX_PROTO ), unsigned timeout );
+
+#endif
 
 #endif
 
